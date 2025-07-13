2ec2d897e4e71d5a976147c740634c6e855e4272
Himel Das <60639301+thehimel@users.noreply.github.com>
2025-07-13 13:45:01 +0200

    Init Nextra project

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com',
+  footer: {
+    text: 'Docs Template',
+  },
+}
+
+export default config
